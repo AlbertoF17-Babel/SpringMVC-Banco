0a959556042ab8cd6b8c687f18f0dd301c3d11af
andres.montaño <andres.montano@babelgroup.com>
2024-04-16 14:01:59 +0200

    parte clase cuenta

--- src/main/java/es/babel/repository/CuentaDB.java
+++ src/main/java/es/babel/repository/CuentaDB.java
@@ -1,7 +1,80 @@
 package es.babel.repository;
 
+import es.babel.model.Cuenta;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CuentaDB implements ICuentaDB {
+
+    List<Cuenta> listaCuentas;
+
+    @Override
+    public Cuenta obtenerCuenta(int id) {
+        return null;
+    }
+
+    @Override
+    public Cuenta agregarCuenta(Cuenta cuenta) {
+        return null;
+    }
+
+    @Override
+    public void modificarCuenta() {
+
+    }
+
+    @Override
+    public void borrarCuenta(int id) {
+
+    }
+
+    @Override
+    public List<Cuenta> listarCuenta() {
+        return null;
+    }
+
+    /*
+    @Override
+    public Cuenta obtenerCuenta(int id) {
+        Cuenta cuenta = null;
+        for (Cuenta cuentaItem : listaCuentas) {
+            if (id == cuentaItem.getId()) {
+                cuenta= cuentaItem;
+                break;
+            }
+        }
+        return cuenta;
+    }
+
+    @Override
+    public Cuenta agregarCuenta(Cuenta cuenta) {
+        listaCuentas.add(cuenta);
+        return cuenta;
+    }
+
+    @Override
+    public void modificarCuenta() {
+
+    }
+
+    @Override
+    public void borrarCuenta(int id) {
+        try{
+            Cuenta cuenta = obtenerCuenta(id);
+            listaCuentas.remove(cuenta);
+        } catch (Exception e){
+            System.out.println("No hay cuenta para borrar");
+        }
+    }
+
+
+    @Override
+    public List<Cuenta> listarCuenta() {
+        return listaCuentas;
+    }
+
+     */
+
 }
--- src/main/java/es/babel/service/CuentaService.java
+++ src/main/java/es/babel/service/CuentaService.java
@@ -1,7 +1,71 @@
 package es.babel.service;
 
+import es.babel.model.Cuenta;
+import es.babel.repository.ClienteDB;
+import es.babel.repository.CuentaDB;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CuentaService implements ICuentaService {
+
+    private final CuentaDB cuentaDB;
+    private final ClienteDB clienteDB;
+
+    public CuentaService(CuentaDB cuentaDB, ClienteDB clienteDB) {
+        this.cuentaDB = cuentaDB;
+        this.clienteDB = clienteDB;
+    }
+
+/*
+    @Override
+    public Cuenta obtenerCuenta(int id, int idCliente) {
+        return cuentaDB.obtenerCuenta(id, idCliente);
+    }
+
+    @Override
+    public Cuenta agregarCuenta(Cuenta cuenta, int idCliente) {
+        return cuentaDB.agregarCuenta(cuenta, idCliente);
+    }
+
+
+ */
+    @Override
+    public Cuenta obtenerCuenta(int id) {
+        return null;
+    }
+
+    @Override
+    public Cuenta agregarCuenta(Cuenta cuenta) {
+        return null;
+    }
+
+    @Override
+    public void modificarCuenta() {
+        cuentaDB.modificarCuenta();
+    }
+
+    @Override
+    public void borrarCuenta(int id) {
+
+    }
+
+    @Override
+    public List<Cuenta> listarCuenta() {
+        return null;
+    }
+    /*
+
+    @Override
+    public void borrarCuenta(int id, int idCliente) {
+        cuentaDB.borrarCuenta(id, idCliente);
+    }
+
+    @Override
+    public List<Cuenta> listarCuenta(int idCliente) {
+        return cuentaDB.listarCuenta(idCliente);
+    }
+
+     */
 }
