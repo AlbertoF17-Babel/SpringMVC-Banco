7e4722bd5b3f2e268ecf04beea61bbf230d64ea9
Mireya Medalle Merino <mireya.medalle@babelgroup.com>
2024-04-16 13:25:57 +0200

    Controller sucursal

--- src/main/java/es/babel/controller/SucursalController.java
+++ src/main/java/es/babel/controller/SucursalController.java
@@ -2,11 +2,13 @@ package es.babel.controller;
 
 import es.babel.model.Sucursal;
 import es.babel.service.SucursalService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.function.SupplierUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -20,11 +22,37 @@ public class SucursalController {
     }
 
     @GetMapping("/sucursales")
-    public String listarSucursales(@ModelAttribute Model model) {
+    public String listarSucursales(Model model) {
         List<Sucursal> sucursales = sucursalService.listarSucursales();
         model.addAttribute("sucursales", sucursales);
         return "sucursales";
     }
+
+    @PostMapping("/sucursal")
+    public String crearSucursal(@ModelAttribute Sucursal sucursal, Model model) {
+        sucursalService.agregarSucursal(sucursal);
+        List<Sucursal> sucursales = sucursalService.listarSucursales();
+        model.addAttribute("sucursales", sucursales);
+        return "sucursales";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String eliminarSucursal(@PathVariable int id, Model model) {
+        sucursalService.borrarSucursal(id);
+        List<Sucursal> sucursales = sucursalService.listarSucursales();
+        model.addAttribute("sucursales", sucursales);
+        return "sucursales";
+    }
+
+    @PostMapping("/sucursal/{id}")
+    public String detalleSucursal(@PathVariable int id, Model model) {
+        Sucursal sucursal = sucursalService.detalleSucursal(id);
+
+        model.addAttribute("sucursal", sucursal);
+        return "detalleSucursal";
+    }
+
+
 
 
 
