06e159bad4cd477ddf432dcc4611629dfebc4e26
AlbertoF17 <albertofergom@gmail.com>
2024-04-23 10:38:12 +0200

    Cuentas y Clientes

--- src/main/java/es/babel/controller/CuentaController.java
+++ src/main/java/es/babel/controller/CuentaController.java
@@ -1,9 +1,17 @@
 package es.babel.controller;
 
+import es.babel.service.IClienteService;
+import es.babel.service.ICuentaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/cliente/cuenta")
+@RequestMapping(value = "/cuentas")
 public class CuentaController {
+
+    private final ICuentaService cuentaService;
+
+    public CuentaController(ICuentaService cuentaService) {
+        this.cuentaService = cuentaService;
+    }
 }
